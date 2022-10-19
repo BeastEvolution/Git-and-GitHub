20fde1cc7086b31064052b13eaf532e4476911e6
Bohdan Lifirenko <qp123456ex@gmail.com>
2022-10-19 15:01:51 +0300

    Add method printTextOnPaper() to class People.

--- src/People.java
+++ src/People.java
@@ -1,8 +1,8 @@
 public class People {
     String Name;
     int age;
-
     Pan pan = new Pan("Read");
+    Paper paper = new Paper();
     public void print()
     {
         System.out.println(Name + ' ' + age);
@@ -12,4 +12,9 @@ public class People {
         System.out.println("Hello");
     }
 
+    public void printTextOnPaper()
+    {
+        pan.write("This is the end.");
+        paper.writeText("This is the end.");
+    }
 }
